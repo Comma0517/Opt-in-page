c7969945126edf087e216522e4a3ee349e0191e1
Comma0517 <Comma0517@gmail.com>
2023-06-28 02:12:59 -0400

    Edit Slide

--- client/src/components/Slide.js
+++ client/src/components/Slide.js
@@ -1,19 +1,39 @@
 import React, { Fragment } from "react";
 import "../App.css";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 import slide1 from "../public/slide1.jpg"
 import slide2 from "../public/slide2.jpg"
 import slide3 from "../public/slide3.jpg"
 
-const Slide = () => {
+const Slideshow  = () => {
   return (
     <Fragment>
-        <div style={{paddingBottom: "30px"}}>
+        {/* <div style={{paddingBottom: "30px"}}>
             <img style={{width: "33.3%"}} src={slide1} alt="slide1"/> 
             <img style={{width: "33.3%"}} src={slide2} alt="slide2"/> 
             <img style={{width: "33.3%"}} src={slide3} alt="slide3"/> 
+        </div> */}
+         <div className="slide-container" style={{paddingBottom: "30px"}}>
+            <Slide>
+                {/* <div>
+                    <img style={{width: "33.3%"}} src={slide1} alt="slide1"/> 
+                    <img style={{width: "33.3%"}} src={slide2} alt="slide2"/> 
+                    <img style={{width: "33.3%"}} src={slide3} alt="slide3"/> 
+                </div> */}
+                <div style={{textAlign: "center"}}>
+                    <img style={{width: "60%"}} src={slide1} alt="slide1"/>
+                </div>
+                <div style={{textAlign: "center"}}>
+                    <img style={{width: "60%"}} src={slide2} alt="slide2"/>
+                </div>
+                <div style={{textAlign: "center"}}>
+                    <img style={{width: "60%"}} src={slide3} alt="slide3"/>
+                </div>
+            </Slide>
         </div>
     </Fragment>
   );
 };
 
-export default Slide;
+export default Slideshow ;
